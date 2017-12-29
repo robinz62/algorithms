143cb8a85e94655ccf45ff5e4ec2bdf845e59927
robinz62 <robinz672@gmail.com>
2017-12-29 17:39:38 -0500

    added overloaded variants of lis()

--- algorithms/LIS.java
+++ algorithms/LIS.java
@@ -1,3 +1,7 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
 /**
  * Implementations of the solution to the Longest Increasing Subsequence problem
  * for various input types where the subsequences are strictly increasing. Runs
@@ -6,26 +10,27 @@
 public class LIS {
     
     /**
-     * Returns a list of the indices of the elements comprising a longest
+     * Returns an array of the indices of the elements comprising a longest
      * increasing subsequence.
+     * 
+     * dp[i] is the index of the smallest element that terminates an increasing
+     * subsequence of length i + 1.
+     * 
+     * to[i] contains the index of the previous element for the increasing
+     * subsequence ending at index i. used for reconstructing the solution.
+     * 
+     * len stores the length of the longest increasing subsequence found.
      */
-    public static int[] lis(int[] arr) {
-        // dp[i] is the index of the smallest element that terminates an
-        // increasing subsequence of length i + 1.
-        Integer[] dp = new Integer[arr.length];
-
-        // to[i] contains the index of the previous element for the increasing
-        // subsequence ending at index i. used for reconstructing the solution.
-        int[] to = new int[arr.length];
-
-        // stores the length of the longest increasing subsequence found.
+    public static <T> int[] lis(T[] nums, Comparator<? super T> c) {
+        Integer[] dp = new Integer[nums.length];
+        int[] to = new int[nums.length];
         int len = 0;
 
-        for (int i = 0; i < arr.length; ++i) {
-            int idx = java.util.Arrays.<Integer>binarySearch(dp, 0, len, i,
-                new java.util.Comparator<Integer>() {
+        for (int i = 0; i < nums.length; ++i) {
+            int idx = Arrays.binarySearch(dp, 0, len, i,
+                new Comparator<Integer>() {
                     public int compare(Integer a, Integer b) {
-                        return Integer.compare(arr[a], arr[b]);
+                        return c.compare(nums[a], nums[b]);
                     }
                 });
             if (idx < 0) {
@@ -50,19 +55,21 @@ public class LIS {
     }
 
     /**
-     * Returns the length of a longest increasing subsequence. Somewhat more
-     * concise due to not needing to reconstruct the solution.
+     * Returns the length of a longest increasing subsequence for when
+     * reconstructing the solution is not necessary. It has a slightly simpler
+     * dynamic programming definition and is much more concise.
+     * 
+     * dp[i] is the smallest element that terminates an increasing subsequence
+     * of length i + 1.
+     * 
+     * len stores the length of the longest increasing subsequence found.
      */
     public static int lengthOfLis(int[] arr) {
-        // dp[i] is the smallest element that terminates an increasing
-        // subsequence of length i + 1.
         int[] dp = new int[arr.length];
-
-        // stores the length of the longest increasing subsequence found.
         int len = 0;
 
         for (int i : arr) {
-            int idx = java.util.Arrays.binarySearch(dp, 0, len, i);
+            int idx = Arrays.binarySearch(dp, 0, len, i);
             if (idx < 0) {
                 idx = -idx - 1;
                 dp[idx] = i;
@@ -72,6 +79,144 @@ public class LIS {
             }
         }
         return len;
+    }
+
+    /*
+     * Overloaded variants of lis(...) are available below for convenience. I
+     * won't bother overloading the lengthOfLis(...) method. The length of
+     * course can be found by taking the length of the result array.
+     */
+
+    public static <T extends Comparable<? super T>> int[] lis(T[] nums) {
+        Integer[] dp = new Integer[nums.length];
+        int[] to = new int[nums.length];
+        int len = 0;
+
+        for (int i = 0; i < nums.length; ++i) {
+            int idx = Arrays.binarySearch(dp, 0, len, i,
+                new Comparator<Integer>() {
+                    public int compare(Integer a, Integer b) {
+                        return nums[a].compareTo(nums[b]);
+                    }
+                });
+            if (idx < 0) {
+                idx = -idx - 1;
+                dp[idx] = i;
+                to[i] = idx > 0 ? dp[idx - 1] : -1;
+                if (idx == len) {
+                    ++len;
+                }
+            }
+        }
+
+        int[] indices = new int[len];
+        int i = indices.length - 1;
+        int curr = dp[len - 1];
+        while (curr != -1) {
+            indices[i--] = curr;
+            curr = to[curr];
+        }
+
+        return indices;
+    }
+
+    public static int[] lis(int[] nums) {
+        Integer[] dp = new Integer[nums.length];
+        int[] to = new int[nums.length];
+        int len = 0;
+
+        for (int i = 0; i < nums.length; ++i) {
+            int idx = Arrays.binarySearch(dp, 0, len, i,
+                new Comparator<Integer>() {
+                    public int compare(Integer a, Integer b) {
+                        return Integer.compare(nums[a], nums[b]);
+                    }
+                });
+            if (idx < 0) {
+                idx = -idx - 1;
+                dp[idx] = i;
+                to[i] = idx > 0 ? dp[idx - 1] : -1;
+                if (idx == len) {
+                    ++len;
+                }
+            }
+        }
+
+        int[] indices = new int[len];
+        int i = indices.length - 1;
+        int curr = dp[len - 1];
+        while (curr != -1) {
+            indices[i--] = curr;
+            curr = to[curr];
+        }
+
+        return indices;
+    }
+
+    public static <T extends Comparable<? super T>> int[] lis(List<T> nums) {
+        Integer[] dp = new Integer[nums.size()];
+        int[] to = new int[nums.size()];
+        int len = 0;
+
+        for (int i = 0; i < nums.size(); ++i) {
+            int idx = Arrays.binarySearch(dp, 0, len, i,
+                new Comparator<Integer>() {
+                    public int compare(Integer a, Integer b) {
+                        return nums.get(a).compareTo(nums.get(b));
+                    }
+                });
+            if (idx < 0) {
+                idx = -idx - 1;
+                dp[idx] = i;
+                to[i] = idx > 0 ? dp[idx - 1] : -1;
+                if (idx == len) {
+                    ++len;
+                }
+            }
+        }
+
+        int[] indices = new int[len];
+        int i = indices.length - 1;
+        int curr = dp[len - 1];
+        while (curr != -1) {
+            indices[i--] = curr;
+            curr = to[curr];
+        }
+
+        return indices;
+    }
+
+    public static <T> int[] lis(List<T> nums, Comparable<? super T> c) {
+        Integer[] dp = new Integer[nums.length];
+        int[] to = new int[nums.length];
+        int len = 0;
+
+        for (int i = 0; i < nums.length; ++i) {
+            int idx = Arrays.binarySearch(dp, 0, len, i,
+                new Comparator<Integer>() {
+                    public int compare(Integer a, Integer b) {
+                        return c.compare(nums.get(a), nums.get(b));
+                    }
+                });
+            if (idx < 0) {
+                idx = -idx - 1;
+                dp[idx] = i;
+                to[i] = idx > 0 ? dp[idx - 1] : -1;
+                if (idx == len) {
+                    ++len;
+                }
+            }
+        }
+
+        int[] indices = new int[len];
+        int i = indices.length - 1;
+        int curr = dp[len - 1];
+        while (curr != -1) {
+            indices[i--] = curr;
+            curr = to[curr];
+        }
+
+        return indices;
     }
 
     public static void main(String[] args) {
