d9cf7ba4c2c11c769e7a109bf1466a0f14a1f08a
Robin Zhang <robinz@qualtrics.com>
2018-07-16 16:47:39 -0700

    uf updates

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 Various data structures and algorithms in Java. Most files contain a `main`
 method that gives a simple demonstration of the algorithm (and can be used to
 verify correctness (hopefully)).
+
+* `/algorithms`: miscellaneous algorithms
+* `/data-structures`: data structures
+  * `/minified`: same data structures but with reduced code length (e.g. no
+    comments, no exception checking) and non-public privacy (for copy pasting).
---
+++ data-structures/minified/UnionFind.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+class UnionFind {
+    private int n;
+    private int numComponents;
+    private int[] parent;
+    private int[] rank;
+
+    public UnionFind(int n) {
+        this.n = numComponents = n;
+        parent = new int[n];
+        rank = new int[n];
+        for (int i = 0; i < n; i++) parent[i] = i;
+    }
+
+    public void union(int u, int v) {
+        int x = find(u);
+        int y = find(v);
+        if (x == y) return;
+        if (rank[x] < rank[y]) parent[x] = y;
+        else if (rank[x] > rank[y]) parent[y] = x;
+        else {
+            parent[x] = y;
+            ++rank[y];
+        }
+        --numComponents;
+    }
+
+    public int find(int u) {
+        int current = u;
+        List<Integer> toUpdate = new ArrayList<>();
+        while (parent[current] != current) {
+            toUpdate.add(current);
+            current = parent[current];
+        }
+        for (Integer node : toUpdate) parent[node] = current;
+        return current;
+    }
+
+    public int getNumComponents() {
+        return numComponents;
+    }
+}
