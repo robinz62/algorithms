f9f8d62df997423da280f04229e874eba42179ac
Robin Zhang <robinz672@gmail.com>
2020-02-19 11:48:37 -0800

    LCA data structure

---
+++ data-structures/minified/LCA.java
@@ -0,0 +1,69 @@
+import java.util.*;
+
+public class LCA {
+
+    private int n;
+    private int[] first;
+    private int[] depth;
+    private int[] st;
+
+    public LCA(List<List<Integer>> adj, int root) {
+        n = adj.size();
+        first = new int[n];
+        depth = new int[n];
+        boolean[] visited = new boolean[n];
+        List<Integer> euler = new ArrayList<>();
+        dfs(adj, root, visited, euler, 0);
+        build(euler);
+    }
+
+    public int lca(int u, int v) {
+        int l = first[u];
+        int r = first[v];
+        if (l > r) {
+            l = first[v];
+            r = first[u];
+        }
+        r++;
+        l += n;
+        r += n;
+        int minNode = st[l];
+        while (l < r) {
+            if ((l & 1) > 0) {
+                minNode = depth[st[l]] < depth[minNode] ? st[l] : minNode;
+                l++;
+            }
+            if ((r & 1) > 0) {
+                r--;
+                minNode = depth[st[r]] < depth[minNode] ? st[r] : minNode;
+            }
+            l /= 2;
+            r /= 2;
+        }
+        return minNode;
+    }
+
+    private void dfs(List<List<Integer>> adj, int u, boolean[] visited,
+            List<Integer> euler, int currDepth) {
+        visited[u] = true;
+        euler.add(u);
+        depth[u] = currDepth;
+        first[u] = euler.size() - 1;
+        for (int v : adj.get(u)) {
+            if (!visited[v]) {
+                dfs(adj, v, visited, euler, currDepth + 1);
+            }
+            euler.add(u);
+        }
+    }
+
+    private void build(List<Integer> euler) {
+        st = new int[euler.size() * 2];
+        for (int i = 0; i < n; i++) {
+            st[n + i] = euler.get(i);
+        }
+        for (int i = n - 1; i > 0; i--) {
+            st[i] = depth[st[i*2]] < depth[st[i*2 + 1]] ? st[i*2] : st[i*2 + 1];
+        }
+    }
+}
