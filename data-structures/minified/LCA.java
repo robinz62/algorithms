7623184433eed8f54cd211d9e3f5934d85641abd
Robin Zhang <robinz672@gmail.com>
2020-06-27 11:04:48 -0700

    lca

--- data-structures/minified/LCA.java
+++ data-structures/minified/LCA.java
@@ -1,5 +1,6 @@
 import java.util.*;
 
+// Segment tree-based LCA.
 class LCA {
     int n;
     int[] first;
@@ -59,10 +60,65 @@ class LCA {
     private void build(List<Integer> euler) {
         st = new int[euler.size() * 2];
         for (int i = 0; i < n; i++) {
-            st[n + i] = euler.get(i);
+            st[n+i] = euler.get(i);
         }
         for (int i = n - 1; i > 0; i--) {
-            st[i] = depth[st[i*2]] < depth[st[i*2 + 1]] ? st[i*2] : st[i*2 + 1];
+            st[i] = depth[st[i*2]] < depth[st[i*2+1]] ? st[i*2] : st[i*2+1];
         }
+    }
+}
+
+// Binary lifting-based LCA
+class LCA {
+    int time = 0;
+    int[] s;
+    int[] f;
+    int[][] up;
+
+    LCA(List<List<Integer>> adj, int root) {
+        s = new int[adj.size()];
+        f = new int[adj.size()];
+        int log2ceil = 31 - Integer.numberOfLeadingZeros(adj.size());
+        up = new int[adj.size()][log2ceil + 1];
+        boolean[] visited = new boolean[adj.size()];
+        visited[root] = true;
+        up[root][0] = root;
+        dfs(adj, visited, root);
+
+        for (int j = 1; j <= log2ceil; j++) {
+            for (int i = 0; i < adj.size(); i++) {
+                int x = up[i][j-1];
+                up[i][j] = x == root ? root : up[x][j-1];
+            }
+        }
+    }
+
+    int lca(int u, int v) {
+        if (u == v) return u;
+        if (isAncestor(u, v)) return u;
+        if (isAncestor(v, u)) return v;
+        int j = up[0].length-1;
+        int curr = u;
+        while (j >= 0) {
+            if (isAncestor(up[curr][j], v)) j--;
+            else curr = up[curr][j];
+        }
+        return up[curr][0];
+    }
+
+    boolean isAncestor(int u, int v) {
+        return s[u] < s[v] && f[u] > f[v];
+    }
+
+    private void dfs(List<List<Integer>> adj, boolean[] visited, int u) {
+        s[u] = time++;
+        for (int v : adj.get(u)) {
+            if (!visited[v]) {
+                visited[v] = true;
+                up[v][0] = u;
+                dfs(adj, visited, v);
+            }
+        }
+        f[u] = time++;
     }
 }
