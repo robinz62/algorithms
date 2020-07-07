ac340b7ec939ad31ea71d8b1015f24ab38ceb571
Robin Zhang <robinz672@gmail.com>
2020-07-06 19:13:42 -0700

    generalize segment tree + support non-commutative operations

--- data-structures/minified/SegmentTree.java
+++ data-structures/minified/SegmentTree.java
@@ -1,7 +1,12 @@
-// For range sum query.
 class SegmentTree {
     int n;
     int[] st;
+
+    // Modify identity and combine together.
+    int identity = 0;
+    int combine(int a, int b) {
+        return a + b;
+    }
 
     SegmentTree(int[] arr) {
         n = arr.length;
@@ -14,14 +19,14 @@ class SegmentTree {
 
     void build() {
         for (int i = n - 1; i > 0; i--) {
-            st[i] = st[i*2] + st[i*2+1];
+            st[i] = combine(st[i*2], st[i*2+1]);
         }
     }
 
     void modify(int i, int value) {
         st[n + i] = value;
         for (i = (n + i) / 2; i > 0; i /= 2) {
-            st[i] = st[i*2] + st[i*2+1];
+            st[i] = combine(st[i*2], st[i*2+1]);
         }
     }
 
@@ -29,107 +34,14 @@ class SegmentTree {
     int query(int l, int r) {
         l += n;
         r += n;
-        int sum = 0;
+        int resl = identity;
+        int resr = identity;
         while (l < r) {
-            if ((l & 1) > 0) {
-                sum += st[l++];
-            }
-            if ((r & 1) > 0) {
-                sum += st[--r];
-            }
+            if ((l & 1) > 0) resl = combine(resl, st[l++]);
+            if ((r & 1) > 0) resr = combine(st[--r], resr);
             l /= 2;
             r /= 2;
         }
-        return sum;
-    }
-}
-
-// For range min query.
-class SegmentTree {
-    int n;
-    int[] st;
-
-    SegmentTree(int[] arr) {
-        n = arr.length;
-        st = new int[n*2];
-        for (int i = 0; i < n; i++) {
-            st[n + i] = arr[i];
-        }
-        build();
-    }
-
-    void build() {
-        for (int i = n - 1; i > 0; i--) {
-            st[i] = Math.min(st[i*2], st[i*2+1]);
-        }
-    }
-
-    void modify(int i, int value) {
-        st[n + i] = value;
-        for (i = (n + i) / 2; i > 0; i /= 2) {
-            asti] = Math.min(st[i*2], st[i*2+1]);
-        }
-    }
-
-    int query(int l, int r) {
-        l += n;
-        r += n;
-        int min = st[l];
-        while (l < r) {
-            if ((l & 1) > 0) {
-                min = Math.min(min, st[l++]);
-            }
-            if ((r & 1) > 0) {
-                min = Math.min(min, st[--r]);
-            }
-            l /= 2;
-            r /= 2;
-        }
-        return sum;
-    }
-}
-
-// For range max query.
-class SegmentTree {
-    int n;
-    int[] st;
-
-    SegmentTree(int[] arr) {
-        n = arr.length;
-        st = new int[n*2];
-        for (int i = 0; i < n; i++) {
-            st[n + i] = arr[i];
-        }
-        build();
-    }
-
-    void build() {
-        for (int i = n - 1; i > 0; i--) {
-            st[i] = Math.max(st[i*2], st[i*2+1]);
-        }
-    }
-
-    void modify(int i, int value) {
-        st[n + i] = value;
-        for (i = (n + i) / 2; i > 0; i /= 2) {
-            st[i] = Math.max(st[i*2], st[i*2+1]);
-        }
-    }
-
-    int query(int l, int r) {
-        l += n;
-        r += n;
-        int max = st[l];
-        while (l < r) {
-            if ((l & 1) > 0) {
-                max = Math.max(max, st[l++]);
-            }
-            if ((r & 1) > 0) {
-                max = Math.max(max, st[--r]);
-            }
-            l /= 2;
-            r /= 2;
-        }
-        return sum;
+        return combine(resl, resr);
     }
 }
