5e2ffd27ef0fddd723e52e86b4ac5eb843c026f7
Robin Zhang <robinz672@gmail.com>
2020-08-04 22:30:27 -0700

    add lazy segtree (finally)

--- data-structures/SegmentTree.java
+++ data-structures/SegmentTree.java
@@ -1,3 +1,8 @@
+// This file contains various segment trees.
+// 1) compact segment tree (from codeforces)
+// 2) standard segment tree with friendly indexing
+// 3) segment tree with range updates
+
 /**
  * Compact segment tree (2n space) based on
  * http://codeforces.com/blog/entry/18051. Doesn't allow more complicated
@@ -47,6 +52,153 @@ class SegmentTree {
             l /= 2;
             r /= 2;
         }
+        return combine(resl, resr);
+    }
+}
+
+/**
+ * A standard segment tree, where node i has children i*2 and i*2+1. This
+ * allows traversing up/down the segment tree as needed for more involved
+ * types of queries.
+ */
+class SegmentTree {
+    int n;
+    int[] st;
+
+    // Modify identity and combine together.
+    int identity = 0;
+    int combine(int a, int b) {
+        return a + b;
+    }
+
+    SegmentTree(int[] arr) {
+        n = arr.length;
+        st = new int[4*n];
+        build(arr, 1, 0, n-1);
+    }
+
+    void build(int[] arr, int i, int l, int r) {
+        if (l == r) st[i] = arr[l];
+        else {
+            int m = l + (r - l) / 2;
+            build(arr, 2*i, l, m);
+            build(arr, 2*i+1, m+1, r);
+            st[i] = combine(st[2*i], st[2*i+1]);
+        }
+    }
+
+    void modify(int i, int value) {
+        modify(1, 0, n-1, i, value);
+    }
+
+    private void modify(int v, int l, int r, int i, int value) {
+        if (l == r) t[v] = value;
+        else {
+            int m = l + (r - l) / 2;
+            if (i <= m) modify(2*v, l, m, i, value);
+            else modify(2*v+1, m+1, r, i, value);
+            t[v] = combine(t[2*v], t[2*v+1]);
+        }
+    }
+
+    // Note: input range is closed [l, r]
+    int query(int l, int r) {
+        return query(1, 0, n-1, l, r);
+    }
+
+    private int query(int v, int l, int r, int ql, int qr) {
+        if (ql > qr) return identity;
+        if (l == ql && r == qr) return st[v];
+        int m = l + (r - l) / 2;
+        int resl = query(2*v, l, m, ql, Math.min(qr, m));
+        int resr = query(2*v+1, m+1, r, Math.max(ql, m+1), qr);
+        return combine(resl, resr);
+    }
+}
+
+/**
+ * Segment tree which supports range updates. Note lazyFunction, which governs
+ * how updates are applied to node values as well as other lazy values.
+ * 
+ * Examples:
+ *   for range add, return currVal + lazyVal
+ *   for range assign, return lazyVal
+ */
+class SegmentTree {
+    int n;
+    int[] st;
+    boolean[] hasLazy;
+    int[] lazy;
+
+    // Adjust identity, combine, and lazyFunction together to suit needs.
+    // Default code below is range sum query with range addition.
+    int identity = 0;
+    int combine(int a, int b) {
+        return a + b;
+    }
+    int lazyFunction(int currVal, int lazyVal) {
+        return currVal + lazyVal;
+    }
+
+    SegmentTree(int[] arr) {
+        n = arr.length;
+        st = new int[4*n];
+        hasLazy = new boolean[4*n];
+        lazy = new int[4*n];
+        build(arr, 1, 0, n-1);
+    }
+
+    private void build(int[] arr, int i, int l, int r) {
+        if (l == r) st[i] = arr[l];
+        else {
+            int m = l + (r - l) / 2;
+            build(arr, 2*i, l, m);
+            build(arr, 2*i+1, m+1, r);
+            st[i] = combine(st[2*i], st[2*i+1]);
+        }
+    }
+
+    private void push(int v) {
+        if (!hasLazy[v]) return;
+        st[2*v] = lazyFunction(st[2*v], lazy[v]);
+        lazy[2*v] = lazyFunction(lazy[2*v], lazy[v]);
+        st[2*v+1] = lazyFunction(st[2*v+1], lazy[v]);
+        lazy[2*v+1] = lazyFunction(lazy[2*v+1], lazy[v]);
+        hasLazy[2*v] = hasLazy[2*v+1] = true;
+        hasLazy[v] = false;
+    }
+
+    void modify(int l, int r, int value) {
+        modify(1, 0, n-1, l, r, value);
+    }
+
+    private void modify(int v, int l, int r, int ql, int qr, int value) {
+        if (ql > qr) return;
+        if (l == ql && r == qr) {
+            st[v] = lazyFunction(st[v], value);
+            lazy[v] = lazyFunction(lazy[v], value);
+            hasLazy[v] = true;
+        } else {
+            push(v);
+            int m = l + (r - l) / 2;
+            modify(2*v, l, m, ql, Math.min(qr, m), value);
+            modify(2*v+1, m+1, r, Math.max(ql, m+1), qr, value);
+            st[v] = combine(st[2*v], st[2*v+1]);
+        }
+    }
+
+    // Note: input range is closed [l, r]
+    int query(int l, int r) {
+        return query(1, 0, n-1, l, r);
+    }
+
+    private int query(int v, int l, int r, int ql, int qr) {
+        if (ql > qr) return identity;
+        if (l == ql && r == qr) return st[v];
+        push(v);
+        int m = l + (r - l) / 2;
+        int resl = query(2*v, l, m, ql, Math.min(qr, m));
+        int resr = query(2*v+1, m+1, r, Math.max(ql, m+1), qr);
         return combine(resl, resr);
     }
 }
